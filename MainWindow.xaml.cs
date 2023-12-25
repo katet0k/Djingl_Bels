26da6ec32b7823963c6763f80c96f2202af401b2
Kateryna <kostucenkokata0@gmail.com>
2023-12-25 15:10:23 +0200

    0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Djingl_Bels
 {
@@ -20,5 +15,8 @@ namespace Djingl_Bels
         {
             InitializeComponent();
         }
+
     }
 }
+
+
