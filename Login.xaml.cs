c2fbd46c4a40b2e6c1b46e0ba11b2bce469e15c5
Даша Побережник <dasosha2017@gmail.com>
2023-12-26 02:52:47 +0200

    ALL

---
+++ Login.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Djingl_Bels
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UserTextBox.Text;
+            string password = PasswordBox.Password;
+
+            // Тут ви можете виконати логіку для перевірки логіну та паролю
+            // Наприклад, звернутися до бази даних або провести перевірку в пам'яті програми
+
+            if (string.IsNullOrEmpty(username))
+            {
+                usernameValidationTextBlockL.Text = "Enter again a login!";
+            }
+            else
+            {
+                usernameValidationTextBlockL.Text = "";
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                passwordValidationTextBlockP.Text = "Enter again a password!";
+            }
+            else
+            {
+                passwordValidationTextBlockP.Text = "";
+            }
+
+            // Логіка перевірки логіну та паролю
+            if (username == "User" && password == "1234")
+            {
+                // Відкрити нове вікно StudentsHome.xaml
+                MainWindow Home = new MainWindow();
+                Home.Show();
+
+                // Закрити поточне вікно Login.xaml
+                this.Close();
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.IO;
 using System.Windows.Media.Imaging;
 
 namespace Djingl_Bels
@@ -78,16 +79,6 @@ namespace Djingl_Bels
             }
         }
 
-        private void SetOriginalImagePosition(Image image)
-        {
-            TranslateTransform transform = image.RenderTransform as TranslateTransform;
-            if (transform != null)
-            {
-                transform.X = 0;
-                transform.Y = 0;
-            }
-        }
-
         private Image CreateNewImage(Image originalImage, Point dropPosition)
         {
             Image newImage = new Image
@@ -96,7 +87,7 @@ namespace Djingl_Bels
                 Width = originalImage.Width,
                 Height = originalImage.Height,
                 RenderTransform = new TranslateTransform(),
-                Margin = new Thickness(dropPosition.X, dropPosition.Y, 0, 0),
+                Margin = new Thickness(dropPosition.X - originalImage.ActualWidth / 2, dropPosition.Y - originalImage.ActualHeight / 2, 0, 0),
             };
 
             Grid parentGrid = (Grid)originalImage.Parent;
@@ -109,9 +100,17 @@ namespace Djingl_Bels
             return newImage;
         }
 
-        
+        private void SetOriginalImagePosition(Image image)
+        {
+            TranslateTransform transform = image.RenderTransform as TranslateTransform;
+            if (transform != null)
+            {
+                transform.X = 0;
+                transform.Y = 0;
+            }
+        }
 
-        // Додано обробник події для кнопки
+        // Додано обробник події для TextBox
         private void ColorCodeTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             // Перевірка чи введений текст може бути розпізнаний як колір
@@ -148,6 +147,45 @@ namespace Djingl_Bels
             }
         }
 
-        
+        private void CaptureScreenshot_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Створюємо бітмап з вмісту екрана
+                RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap((int)this.Width, (int)this.Height, 96, 96, PixelFormats.Pbgra32);
+                renderTargetBitmap.Render(this);
+
+                // Створюємо кодек для формату PNG
+                PngBitmapEncoder pngImage = new PngBitmapEncoder();
+                pngImage.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+
+                // Зберігаємо знімок екрана в файл
+                string screenshotPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NewYearThree.png");
+                using (FileStream fileStream = new FileStream(screenshotPath, FileMode.Create))
+                {
+                    pngImage.Save(fileStream);
+                }
+
+                MessageBox.Show($"Знімок екрана збережено за шляхом: {screenshotPath}", "Успіх", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при збереженні знімка екрана: {ex.Message}", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ClearField_Click(object sender, RoutedEventArgs e)
+        {
+            // Створюємо новий екземпляр MainWindow
+            MainWindow newMainWindow = new MainWindow();
+
+            // Закриваємо поточний екземпляр MainWindow
+            this.Close();
+
+            // Відображаємо новий екземпляр MainWindow
+            newMainWindow.Show();
+        }
+
+
     }
 }
